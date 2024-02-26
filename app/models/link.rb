7e3c3370cd7af29493489581d553327d556651fb
Alobeast <alois.beard@gmail.com>
2024-02-26 22:34:57 +0100

    Use Base62 encoded Link ids in url

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,12 @@ class Link < ApplicationRecord
   validates :url, presence: true
 
   scope :recent_first, -> { order(created_at: :desc) }
+
+  def self.find(id)
+    super Base62.decode(id)
+  end
+
+  def to_param
+    Base62.encode(id)
+  end
 end
